00d90f63aeb07263a5009adca2987a575edc2c2b
NghiaTV3 <trannghi170735@gmail.com>
2026-03-20 23:56:47 +0700

    Creating Dashboard & No Match 404 components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
+import Dashboard from "./pages/header/dashboard/Dashboard";
 import Header from "./pages/header/Header";
+import NoMatch from "./pages/header/noMatch/NoMatch";
 
 function App() {
    return (
       <div className="App">
          <>
             <Header />
+            <Routes>
+               <Route path="/" element={<Dashboard />} />
+               <Route path="*" element={<NoMatch />} />
+            </Routes>
          </>
       </div>
    );
